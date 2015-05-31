1b98dcf67c375f56003076a4fb1888520521b2b0
Constanti <constantin.dumba@gmail.com>
2015-05-30 14:24:33 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
+  return {
+    setValue : function(key,value){
+      this.key = value;
+    },
+    getValue : function(key){
+      if(this.key){
+        return this.key;
+      } else return null;
+    }
+  };
 };
